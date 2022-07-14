05798a7e6673649272f09558a7a0d4e660e52345
931507706@qq.com <931507706@qq.com>
2022-07-14 18:34:51 +0800

    init

---
+++ src/CaptchaServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dianthink\Captcha;
+
+use Illuminate\Support\ServiceProvider;
+/**
+ * Class CaptchaServiceProvider
+ * @package Youngyezi\Captcha
+ */
+class CaptchaServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Bind captcha
+        $this->app->bind('captcha', function($app)
+        {
+            return new Captcha($app);
+        });
+    }
+}
